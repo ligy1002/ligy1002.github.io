a63a28690c00b01ef6ac3e3c4d6000c0da4d2d2a
ligy1002 <38318209+ligy1002@users.noreply.github.com>
2018-04-18 11:32:18 +0800

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,15 @@
+function showPic(whichpic){
+	                
+        var source = whichpic.getAttribute("href");
+	                  
+        var placeholder = document.getElementById("placeholder");
+	
+        placeholder.setAttribute("src",source);
+	
+	var text = document.getAttribute("title");
+	
+	var description = document.getElementById("description");
+	
+	description.firstChild.nodeValue = text;
+         
+                           }
