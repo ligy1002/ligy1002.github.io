ce2e8cfe6a289f90998c89f5c5c9ca5149ffc482
ligy1002 <38318209+ligy1002@users.noreply.github.com>
2018-04-28 10:19:21 +0800

    Update 5.js

--- five/5.js
+++ five/5.js
@@ -1,11 +1,11 @@
 function popUp(winURL){
-	window.open(winURL,"popUp","width=1000,height=1000");
+	window.open(winURL,"popup","width=1000,height=1000");
 }
 window.onload = prepareLinks;
 function prepareLinks(){
 	var links = document.getElementByTagName("a");
 	for(var i=0;i<links.length;i++){
-		if(links[i].getAttribute("class") == "popUp"){
+		if(links[i].getAttribute("class") == "popup"){
 			links[i].onclick = function(){
 				popUp(this.getAttribute("href"));
 				return false;
